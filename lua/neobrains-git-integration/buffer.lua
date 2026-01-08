e362b59ef319aaa387f3d3af41a1e61711dad0f5
SCRIPTERBLOX <scriptoblox@gmail.com>
2026-01-08 09:15:19 +0000

    Automatic window detection

--- lua/neobrains-git-integration/buffer.lua
+++ lua/neobrains-git-integration/buffer.lua
@@ -1,6 +1,6 @@
 local buffer = {}
 
-function buffer.create(cfg, win)
+function buffer.create(cfg)
 	local buf = vim.api.nvim_create_buf(false, true)
 
 	local lines = {
@@ -9,6 +9,8 @@ function buffer.create(cfg, win)
 		"Integration"
 	}
 
+	vim.cmd(":NvimTreeFocus")
+	local win = vim.api.nvim_get_current_win()
 	vim.api.nvim_win_set_buf(win, buf)
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 	vim.api.nvim_buf_set_option(buf, "modifiable", false)
--- lua/neobrains-git-integration/init.lua
+++ lua/neobrains-git-integration/init.lua
@@ -2,10 +2,10 @@ local buffer = require("neobrains-git-integration.buffer")
 
 local M = {}
 
-function M.create(user_config, tree_win)
+function M.create(user_config)
 	local cfg = vim.tbl_deep_extend("force", require("neobrains-git-integration.config").default_config, user_config or {})
 
-	local buf = buffer.create(cfg, tree_win)
+	local buf = buffer.create(cfg)
 
 	return buf
 end
